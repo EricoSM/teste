d0e461838a7d7168ed2be4100104a59f03d03369
Erico Augusto <erico_au@hotmail.com>
2020-04-08 16:03:19 -0300

    enum

---
+++ Enumeracoes/Enum_e_1/Enum_e_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Enum_e_1.Entities;
+using Enum_e_1.Entities.Enums;
+using System;
+
+namespace Enum_e_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Order order = new Order
+            {
+                Id = 1899,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();  //order enumerada para string
+
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
