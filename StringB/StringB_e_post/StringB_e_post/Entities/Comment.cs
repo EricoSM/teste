7db29153dea0354be7767001d44f757600a0f593
Erico Augusto <erico_au@hotmail.com>
2020-04-10 15:58:39 -0300

    StringBuilder

---
+++ StringB/StringB_e_post/StringB_e_post/Entities/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringB_e_post.Entities
+{
+    class Comment
+    {
+
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ StringBuilder/StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Ex_stringbuilder.Entities;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow that´s awesome!");
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I´m going to visit this wonderful country!",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+
+
+        }
+    }
+}
